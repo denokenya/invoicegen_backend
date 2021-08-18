f4f19babff4abf8f51df49890f64ee88fe9af32d
shrionit <sptiwari46289@gmail.com>
2021-08-18 20:56:11 +0530

    fixed secret key

--- invoicegen_backend/settings.py
+++ invoicegen_backend/settings.py
@@ -25,10 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR, "etc/secret.key")) as f:
-    SECRET_KEY = f.read().strip()
 
-SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
+SECRET_KEY = os.environ.get("SECRET_KEY", "q^_+$%=hlvkr6ctnd*n&btx1=xgqg43lbio_@rs15z(v-7um5*")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
