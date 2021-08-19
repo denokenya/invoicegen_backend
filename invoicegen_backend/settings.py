d29f8ae8d9b6b29018bea0cd7ea0ac515bddab2b
shrionit <sptiwari46289@gmail.com>
2021-08-19 14:25:00 +0530

    testing

--- invoicegen_backend/settings.py
+++ invoicegen_backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "gunicorn"
+    "gunicorn",
     "rest_framework",
     "api.apps.ApiConfig",
     "account.apps.AccountConfig",
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
